566b14d0a902cfc0d353cd87c1b4033b3394665a
Francis MudzungayirI <fmudzungayiri@gmail.com>
2025-03-04 21:59:02 +0200

    habit tracker solution

---
+++ habit tracker/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+import datetime
+
+
+
+app = Flask(__name__)
+
+habits = []
+
+
+@app.context_processor
+def add_calc_date():
+    
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        
+        return dates 
+    return {'date_range': date_range}
+
+
+
+@app.route('/')
+def index():
+    date_str = request.args.get('date')
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+    
+    return render_template(
+        'index.html', 
+        habits = habits, 
+        title = 'Habit Tracker | Home',
+        selected_date = selected_date
+    )
+
+@app.route('/add', methods = ['GET', 'POST'])
+def add_habit():
+    if request.method =='POST':
+        habit = request.form.get('habit')
+        habits.append(habit)
+        print(habits)
+    return render_template(
+        'add_habit.html', 
+        title = 'Habit Tracker | Add Habit',
+        selected_date =datetime.date.today()
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
